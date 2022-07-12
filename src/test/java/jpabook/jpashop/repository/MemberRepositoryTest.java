66540e5c3dc1640ccb9c42225f7f21d4f96b1f65
choiihwa <chldlghk@gmail.com>
2022-07-12 22:13:49 +0900

    Create member logic.

---
+++ src/test/java/jpabook/jpashop/repository/MemberRepositoryTest.java
@@ -0,0 +1,14 @@
+package jpabook.jpashop.repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MemberRepositoryTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
